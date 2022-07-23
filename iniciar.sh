6888d6525396ab84cc6782084530f770be4b3032
dione566 <77499190+dione566@users.noreply.github.com>
2022-07-23 13:00:14 -0300

    Update iniciar.sh

--- iniciar.sh
+++ iniciar.sh
@@ -4,17 +4,17 @@ clear
 nums=$(seq 100 299 | tr -s '\n' '|')
 nums+=$(seq -s"|" 400 499)
 
-echo -e "\e[01;33mATESTE 1 \e[0m\n\n\e[02mEssa ferramenta irá fazer solicitações\npara um domínio específico\nutilizando um range de ip que você inserir\nutilize com Dados móveis sem créditos\n\nFormato proxy: 10.10. ou 10.10.10.\nEle irá fazer varias solicitações\nadicionando de 0 à 255 após o .\e[0m\n"; sleep 1
+echo -e "\e[01;33mATESTE 2 \e[0m\n\n\e[02mEssa ferramenta irá fazer solicitações\npara um domínio específico\nutilizando um range de ip que você inserir\nutilize com Dados móveis sem créditos\n\nFormato proxy: 10.10. ou 10.10.10.\nEle irá fazer varias solicitações\nadicionando de 0 à 255 após o .\e[0m\n"; sleep 1
 
 while :; do
 echo -e "\e[01;37mColoque o IP/Proxy que deseja utilizar\e[0m"
 read -p "IP/Proxy: " IP
 echo -e "\e[01;37mColoque o PORTA que deseja utilizar\e[0m"
+echo"80"
 read -p "IP/Proxy: " PORTA
-read -p " -e -i 80 resp\e[0m"
 echo -e "\e[01;37mColoque o seu DOMÍNIO\e[0m"
+echo"salve.gomesnetbr.xyz"
 read -p "Domínio: " DOMINIO
-read -p " -e -i salve.gomesnetbr.xyz resp\e[0m"
 
 dots=$(fgrep "." -o <<< $IP | wc -l)
 > ips.txt; echo "@EhisOpeNer" > a; > b
