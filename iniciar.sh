d04c4c803baf49ce7936f11757789673320dd4f3
dione566 <77499190+dione566@users.noreply.github.com>
2022-07-23 11:46:53 -0300

    Update iniciar.sh

--- iniciar.sh
+++ iniciar.sh
@@ -7,7 +7,7 @@ nums+=$(seq -s"|" 400 499)
 echo -e "\e[01;33mPROCURAR PROXYS \e[0m☠\n\n\e[02mEssa ferramenta irá fazer solicitações\npara um domínio específico\nutilizando um range de ip que você inserir\nutilize com Dados móveis sem créditos\n\nFormato proxy: 10.10. ou 10.10.10.\nEle irá fazer varias solicitações\nadicionando de 0 à 255 após o .\e[0m\n"; sleep 1
 
 while :; do
-echo -ne "\033[1;32mQUAL IP/PROXY?\033[1;31m ?\033[1;37m : "
+echo -ne "\033[1;32mQUAL IP/PROXY?\033[1;31m : "
 read -p "IP/Proxy: " IP
 echo -ne "\033[1;32mQUAL PORTA?\033[1;31m 80"
 read -p "IP/Proxy: " PORTA
