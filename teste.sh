e10313fdaa77c02646c10e8d6d0667a0f14c36ae
dione566 <77499190+dione566@users.noreply.github.com>
2025-04-19 08:57:56 -0400

    Update teste.sh

--- teste.sh
+++ teste.sh
@@ -1,7 +1,7 @@
-echo "Iniciando Bot Prezao Free Hack"
+echo "🅸🅽🅸🅲🅸🅰🅽🅳🅾 🅱🅾🆃 🅿🆁🅴🆉🅰🅾 🅵🆁🅴🅴 🅷🅰🅲🅺"
 apt-get update
 pip install -r requirements.txt
 cd /sdcard/Download/Telegram/Script
 python main.py
 sleep 2
-echo "DONE!!!"
+echo "🅱🅾🆃 🅸🅽🅸🅲🅸🅰🅳🅾 🅲🅾🅼 🆂🆄🅲🅴🆂🆂🅾 ..."
