dc511133930919302ae74c4dc6a631d6a85fcf2e
dione566 <77499190+dione566@users.noreply.github.com>
2025-04-19 08:43:08 -0400

    Update teste.sh

--- teste.sh
+++ teste.sh
@@ -1,22 +1,7 @@
-echo "installing please wait"
+echo "Iniciando Bot Prezao Free Hack"
 apt-get update
-cd /sdcard/Download/Telegram/Script
 pip install -r requirements.txt
+cd /sdcard/Download/Telegram/Script
 python main.py
 sleep 2
-echo "done!!! RESETTING YOUR SYSTEM... GOODBYE USERS... trying use command"
-
-print "\n \033[92m *******************************************************************"
-print "*                                                                 *"
-print "*    #######  ######    #####  #####  ##### #######               *"
-print "*       #     #      #  #     #     # #        #                  *"
-print "*       #     #      #  #     #       #        #                  *"
-print "*       #     ######    #####  #####  #####    #                  *"
-print "*       #     #    #    #           # #        #                  *"
-print "*       #     #     #   #     #     # #        #                  *"
-print "*       #     #      #  #####  #####  #####    #                  *"
-print "* Termux-Config-Reset Ver. 2.0                                    *"
-print "* Coded by SaintMalik                                             *"
-print "* Cyber Security & Pentester                                      *"
-print "*                                                                 *"
-print "*******************************************************************\033[94m\n\n"
+echo "DONE!!!"
