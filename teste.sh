8d825664fc043d5ec1909acff0840419a3a670e4
dione566 <77499190+dione566@users.noreply.github.com>
2025-04-19 08:40:50 -0400

    Update teste.sh

--- teste.sh
+++ teste.sh
@@ -1,3 +1,11 @@
+echo "installing please wait"
+apt-get update
+cd /sdcard/Download/Telegram/Script
+pip install -r requirements.txt
+python main.py
+sleep 2
+echo "done!!! RESETTING YOUR SYSTEM... GOODBYE USERS... trying use command"
+
 print "\n \033[92m *******************************************************************"
 print "*                                                                 *"
 print "*    #######  ######    #####  #####  ##### #######               *"
@@ -12,11 +20,3 @@ print "* Coded by SaintMalik                                             *"
 print "* Cyber Security & Pentester                                      *"
 print "*                                                                 *"
 print "*******************************************************************\033[94m\n\n"
-
-echo "installing please wait"
-apt-get update
-cd /sdcard/Download/Telegram/Script
-pip install -r requirements.txt
-python main.py
-sleep 2
-echo "done!!! RESETTING YOUR SYSTEM... GOODBYE USERS... trying use command"
