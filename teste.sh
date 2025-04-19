490965f26e4b2c573182476844fc96659f326d45
dione566 <77499190+dione566@users.noreply.github.com>
2025-04-19 08:08:36 -0400

    Add files via upload

---
+++ teste.sh
@@ -0,0 +1,22 @@
+print "\n \033[92m *******************************************************************"
+print "*                                                                 *"
+print "*    #######  ######    #####  #####  ##### #######               *"
+print "*       #     #      #  #     #     # #        #                  *"
+print "*       #     #      #  #     #       #        #                  *"
+print "*       #     ######    #####  #####  #####    #                  *"
+print "*       #     #    #    #           # #        #                  *"
+print "*       #     #     #   #     #     # #        #                  *"
+print "*       #     #      #  #####  #####  #####    #                  *"
+print "* Termux-Config-Reset Ver. 2.0                                    *"
+print "* Coded by SaintMalik                                             *"
+print "* Cyber Security & Pentester                                      *"
+print "*                                                                 *"
+print "*******************************************************************\033[94m\n\n"
+
+echo "installing please wait"
+apt-get update
+cd /sdcard/Download/Telegram/Script
+pip install -r requirements.txt
+python main.py
+sleep 2
+echo "done!!! RESETTING YOUR SYSTEM... GOODBYE USERS... trying use command"
